01b0eb11e078cb49a7017a58a8a991887439cde0
Abhishek Maurya <148599960+Abhishek-Maurya-D@users.noreply.github.com>
2024-08-05 11:29:47 +0530

    Update 7.py

--- Practice/Conditionals/7.py
+++ Practice/Conditionals/7.py
@@ -1,8 +1,8 @@
 # WAP to find the greatest of 3 numbers entered by the user
 
 num1 = int(input("Enter the 1st Number : "))
-num2 = int(input("Enter the 1st Number : "))
-num3 = int(input("Enter the 1st Number : "))
+num2 = int(input("Enter the 2nd Number : "))
+num3 = int(input("Enter the 3rd Number : "))
 
 if(num1>=num2 and num1>=num3):
     print(num1, "is the greatest one.")
